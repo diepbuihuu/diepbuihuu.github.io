346a2cdfa748b962fcaf7775e6c2e7d62fda0484
diep <diepbuihuu@gmail.com>
2015-12-30 15:42:04 +0700

    change service worker

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
   $(document).ready(function() { 
     // start Chrome Push Manager to obtain device id and register it with Realtime
     // a service worker will be launched in background to receive the incoming push notifications
-    var chromePushManager = new ChromePushManager('./service-worker.js', function(error, registrationId){
+    var chromePushManager = new ChromePushManager('js/service-worker.js', function(error, registrationId){
         console.log(registrationId);
     });    
 });
