470de31cb18292a91ea6f6ee45ab6369a0491f92
diep <diepbuihuu@gmail.com>
2016-01-07 18:12:02 +0700

    check moment again

--- index2.js
+++ index2.js
@@ -1,4 +1,6 @@
   $(document).ready(function() { 
-    var diff = moment().diff(moment('2016-01-07T11:15:00.000Z'));
-    alert(diff);
+        var testMoment = moment('2016-01-07T11:15:00.000Z');
+        alert (testMoment.format('h:mmA'));
+        var diff = testMoment.fromNow();
+        alert(diff);
 });
