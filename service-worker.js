5a65f702d886ee9a109e317baed56a61f9b18f1a
diep <diepbuihuu@gmail.com>
2016-01-21 11:18:09 +0700

    final fix service worker

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,70 @@
 var guid = '';
+var meetId = '';
 
-self.addEventListener('push', function(event) {  
-  console.log('Received a push message', event);
+self.addEventListener('push', function (event) {
 
-  var title = event.title + guid;  
-  var body = event.body;  
-  var icon = 'https://raw.githubusercontent.com/deanhume/typography/gh-pages/icons/typography.png';  
-  var tag = 'socialivo';
-  
-  event.waitUntil(  
-    self.registration.showNotification(title, {  
-       body: body,  
-       icon: icon,  
-       tag: tag  
-     })  
-   );  
+    event.waitUntil(
+        fetch('http://localhost:8080/getLastNotification?guid=' + guid).then(function (response) {
+            try {
+                var data = JSON.parse(response);
+                var title = data.title || 'New notification';
+                var body = data.body || 'There is newly updated content available on the site. Click to see more.';
+                meetId = data.meetId || '';
+                var icon = 'https://www.socialivo.com/design/image/favicon.png';
+                var tag = 'socialivo';
+                self.registration.showNotification(title, {  
+                    body: body,  
+                    icon: icon,  
+                    tag: tag  
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        }).catch(function (err) {
+            self.registration.showNotification('New notification', {
+                body: 'There is newly updated content available on the site. Click to see more.',
+                icon: 'https://www.socialivo.com/design/image/favicon.png',
+                tag: 'socialivo'
+            });
+        })
+
+    );
 });
 
 self.addEventListener('message', function (evt) {
-  console.log('postMessage received', evt.data);
-  if (evt.data.action === 'send guid') {
-      guid = evt.data.guid;
-  }
-  evt.ports[0].postMessage({'test': 'This is my response.'});
+    console.log('postMessage received', evt.data);
+    if (evt.data.action === 'send guid') {
+        guid = evt.data.guid;
+    }
+    evt.ports[0].postMessage({'test': 'This is my response.'});
 })
 
-self.addEventListener('notificationclick', function(event) {  
-  console.log('On notification click: ', event.notification.tag);  
-  // Android doesn't close the notification when you click on it  
-  // See: http://crbug.com/463146  
-  event.notification.close();
+self.addEventListener('notificationclick', function (event) {
+    console.log('On notification click: ', event.notification.tag);
+    // Android doesn't close the notification when you click on it  
+    // See: http://crbug.com/463146  
+    event.notification.close();
 
-  // This looks to see if the current is already open and  
-  // focuses if it is  
-  event.waitUntil(
-    clients.matchAll({  
-      type: "window"  
-    })
-    .then(function(clientList) {  
-      for (var i = 0; i < clientList.length; i++) {  
-        var client = clientList[i];  
-        if (client.url == '/' && 'focus' in client)  
-          return client.focus();  
-      }  
-      if (clients.openWindow) {
-        return clients.openWindow('https://www.socialivo.com');  
-      }
-    })
-  );
+    // This looks to see if the current is already open and  
+    // focuses if it is  
+    event.waitUntil(
+            clients.matchAll({
+                type: "window"
+            })
+            .then(function (clientList) {
+                for (var i = 0; i < clientList.length; i++) {
+                    var client = clientList[i];
+                    if (client.url == '/' && 'focus' in client)
+                        return client.focus();
+                }
+                if (clients.openWindow) {
+                    if (meetId) {
+                        return clients.openWindow('https://www.socialivo.com/activity.html?act=' + meetId);
+                    } else {
+                        return clients.openWindow('https://www.socialivo.com');
+                    }
+                    
+                }
+            })
+            );
 });
